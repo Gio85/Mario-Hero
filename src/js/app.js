be8996a5aa63576295fa4b3a47bf2639e3b2a5c0
Gio85 <giovanni.galiero85@gmail.com>
2017-08-25 15:11:27 +0100

    Initial project

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,29 @@
-console.log('testing 1 2 3 ');
+$(() => {
+  let timerId = null;
+
+  const $balls = $('.circle');
+  let marginTop = 0;
+  const $button = $('button');
+  let score = 0;
+  const $score = $('.score');
+
+  function runningTimer(){
+    timerId = setInterval(() => {
+      if (marginTop === 550){
+        clearInterval(timerId);
+      } else {
+        $balls.css('margin-top', marginTop + 1);
+        marginTop++;
+        console.log(marginTop);
+      }
+    },10);
+  }
+  $button.on('click',runningTimer);
+  $balls.on('click', (e)=>{
+    $(e.target).css('visibility', 'hidden');
+    score++;
+    $score.text(score);
+
+  });
+
+}); //leave this
