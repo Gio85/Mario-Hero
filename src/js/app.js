a4401e2a062ff2b0a025b3c7db43df46e88c4cc1
Gio85 <giovanni.galiero85@gmail.com>
2017-08-26 17:39:54 +0100

    added moving methonds to mario

--- src/js/app.js
+++ src/js/app.js
@@ -4,38 +4,54 @@ $(() => {
   const $button = $('button');
   const $score = $('.score');
   const timers = [];
+  // const $mario = $('.mario');
+  const $skinny = $('.skinny');
+  $(document).keydown(function(event) {
+    switch (event.keyCode) {
+      case 37: return changingClassLeft();
+      case 39: return changingClassRight();
+    }
+  });
+
+  function changingClassRight(){
+    const $mario = $('.mario');
+    if($mario.index('.skinny') === 3) return false;
+    $mario.next('.skinny').addClass('mario');
+    $mario.removeClass('mario');
+  }
+  function changingClassLeft(){
+    const $mario = $('.mario');
+    if($mario.index('.skinny') === 0) return false;
+    $mario.prev('.skinny').addClass('mario');
+    $mario.removeClass('mario');
+  }
 
   function dropBall($ball, speed){
-    let marginTop = 0;
-    const timerId = setInterval(() => {
-      if (marginTop === 550){
-        clearInterval(timerId);
-        // $ball.css('visibility','hidden');
+    const marginTop = parseFloat($ball.css('margin-top'));
+    const timerId = setTimeout(() => {
+      if (marginTop >= 550){
+        $ball.css('margin-top', 0);
+        speed = Math.ceil(Math.random() * 10) + 10;
       } else {
         $ball.css('margin-top', marginTop + 1);
-        marginTop++;
       }
+      dropBall($ball, speed);
     },speed);
     timers.push(timerId);
   }
+
   function startButton(){
-    let time = 0;
-    const timerIdStartButton = setInterval(() => {
-      if (time === 10){
-        clearInterval(timerIdStartButton);
-      } else {
-        repeatingDroppinBall();
-        time++;
-      }
-    }, 2000);
+    // timers.forEach(timerId => clearInterval(timerId));
+    $balls.toArray().forEach((ball, index) => {
+      setTimeout(() => {
+        dropBall($(ball), Math.ceil(Math.random() * 10) + 10);
+      }, 1000 * index);
+    });
   }
 
-  function repeatingDroppinBall(){
-    timers.forEach(timerId => clearInterval(timerId));
-    $balls.toArray().forEach(ball => dropBall($(ball), Math.floor(Math.random() * 10)));
-  }
 
-  $button.on('click',startButton);
+
+  $button.on('click', startButton);
 
   $balls.on('click', ()=>{
     score++;
