122980631fb491ae84e4114bd383394f51723880
Gio85 <giovanni.galiero85@gmail.com>
2017-08-26 19:22:52 +0100

    added audio theme to the page

--- src/js/app.js
+++ src/js/app.js
@@ -1,48 +1,59 @@
 $(() => {
   let score = 0;
-  const $balls = $('.circle');
+  const $coin = $('.coins');
   const $button = $('button');
   const $score = $('.score');
   const timers = [];
+  const $themeSong = $('audio');
+  $themeSong.play();
   // const $mario = $('.mario');
-  const $skinny = $('.skinny');
+  // const $skinny = $('.skinny');
   $(document).keydown(function(event) {
     switch (event.keyCode) {
-      case 37: return changingClassLeft();
-      case 39: return changingClassRight();
+      case 37: return changingClassToLeft();
+      case 39: return changingClassToRight();
     }
   });
-
-  function changingClassRight(){
+  function winCheck(){
+    const marginTop = parseFloat($coin.css('margin-top'));
+    // const $mario = $('.mario');
+    if(marginTop===400){
+      score++;
+      $score.text(score);
+    } else{
+      $coin.css('margin-top', marginTop + 1);
+    }
+    console.log(marginTop);
+  }
+  function changingClassToRight(){
     const $mario = $('.mario');
     if($mario.index('.skinny') === 3) return false;
     $mario.next('.skinny').addClass('mario');
     $mario.removeClass('mario');
   }
-  function changingClassLeft(){
+  function changingClassToLeft(){
     const $mario = $('.mario');
     if($mario.index('.skinny') === 0) return false;
     $mario.prev('.skinny').addClass('mario');
     $mario.removeClass('mario');
   }
 
-  function dropBall($ball, speed){
-    const marginTop = parseFloat($ball.css('margin-top'));
+  function dropBall($coin, speed){
+    const marginTop = parseFloat($coin.css('margin-top'));
     const timerId = setTimeout(() => {
-      if (marginTop >= 550){
-        $ball.css('margin-top', 0);
+      if (marginTop >= 500){
+        $coin.css('margin-top', 0);
         speed = Math.ceil(Math.random() * 10) + 10;
       } else {
-        $ball.css('margin-top', marginTop + 1);
+        $coin.css('margin-top', marginTop + 1);
       }
-      dropBall($ball, speed);
+      dropBall($coin, speed);
     },speed);
     timers.push(timerId);
   }
 
   function startButton(){
-    // timers.forEach(timerId => clearInterval(timerId));
-    $balls.toArray().forEach((ball, index) => {
+    $coin.toArray().forEach((ball, index) => {
       setTimeout(() => {
         dropBall($(ball), Math.ceil(Math.random() * 10) + 10);
       }, 1000 * index);
@@ -51,11 +62,8 @@ $(() => {
 
 
 
-  $button.on('click', startButton);
-
-  $balls.on('click', ()=>{
-    score++;
-    $score.text(score);
+  $button.on('click', () =>{
+    startButton();
+    winCheck();
   });
-
 }); //leave this
