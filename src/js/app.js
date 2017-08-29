c9263035185d27c3f407138fb550ba63620825ed
Gio85 <giovanni.galiero85@gmail.com>
2017-08-29 14:58:13 +0100

    modified lose condition

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,7 @@
 $(() => {
+  let num = 10;
+  let level = 1;
+  let numMargin = 1;
   let score = 0;
   const $coin = $('.coins');
   const $button = $('button');
@@ -9,106 +12,122 @@ $(() => {
   const $intro = $('.instructions');
   const $level = $('.level');
 
-  $(document).keydown(function(event) {
-    switch (event.keyCode) {
-      case 37: return changingClassToLeft();
-      case 39: return changingClassToRight();
-    }
-  });
+  function changingClass(index, direction){
+    const $mario = $('.mario');
+    if($mario.index('.skinny') === index) return false;
 
-  function changingClassToRight(){
-    const $mario = $('.mario');
-    if($mario.index('.skinny') === 3) return false;
-    $mario.next('.skinny').addClass('mario');
-    // $mario.next('.skinny').css('background-image', 'url(../images/mario-running-right.gif)');
-    $mario.removeClass('mario');
+    const $next = direction === 'left' ? $mario.prev() : $mario.next();
+    $next.addClass('mario ' + direction);
+    $mario.removeClass('mario right left');
   }
-  function changingClassToLeft(){
-    const $mario = $('.mario');
-    if($mario.index('.skinny') === 0) return false;
-    $mario.prev('.skinny').addClass('mario');
-    // $mario.prev('.skinny').css('background-image', 'url(../images/mario-running-left.gif)');
-    $mario.removeClass('mario');
+
+
+  function changingLevel(){
+    level++;
+    $level.text(level);
   }
-  let num = 10;
-  let level = 1;
-  let numMargin = 1;
-  function dropBall($coin, speed){
+  function hidingClasses(){
+    $grid.hide();
+    $level.hide();
+    $score.hide();
+    $('h2').hide();
+  }
+  function displayingScore(){
+    score++;
+    $score.text(score);
+  }
+
+  function checkingScore(){
+    console.log('inside checkingScore', score);
+    switch(score){
+      case 10:
+        changingLevel();
+        num-=1;
+        break;
+      case 20:
+        changingLevel();
+        num-=2;
+        break;
+      case 30:
+        changingLevel();
+        num-=3;
+        break;
+      case 50:
+        changingLevel();
+        num-=4;
+        break;
+      case 70:
+        changingLevel();
+        num-=5;
+        break;
+      case 80:
+        changingLevel();
+        // numMargin++;
+        num-=6;
+        break;
+      case 90:
+        changingLevel();
+        num-=9;
+        break;
+      case 100:
+        console.log('inside case 10', score);
+        $('.final').css('visibility', 'visible');
+        hidingClasses();
+        break;
+    }
+  }
+  function loseCondition(){
+    if(score<0){
+      $('.lose').css('visibility', 'visible');
+      hidingClasses();
+      return true;
+    }
+    return false;
+  }
+
+  function dropCoins($coin, speed){
     const marginTop = parseFloat($coin.css('margin-top'));
     const timerId = setTimeout(() => {
-      if (marginTop >= $skinny.height() - $coin.height()){
+      if(marginTop >= $skinny.height() - $coin.height()) {
         // coin has hit floor
         $coin.css('margin-top', 0);
-        speed = Math.ceil(Math.random() * 10) + num;//decrease the number by 1 for next level
+        speed = generateSpeed(num);//decrease num by 1 for next level
         score--;
-        $score.text(score);
       } else if($coin.parent().hasClass('mario') && marginTop >= $skinny.height() - 120) {//120 is a random given number
         // coin has hit mario
-        score++;
-        if(score===10){
-          level++;
-          $level.text(level);
-          num-=7;
-
-        } else if(score===22){
-          level++;
-          $level.text(level);
-          num-=1;
-          console.log(score, num, level);
-        } else if(score===30){
-          level++;
-          $level.text(level);
-          numMargin++;
-        } else if(score===50){
-          level++;
-          $level.text(level);
-        }else if(score===70){
-          level++;
-          $level.text(level);
-          numMargin++;
-          num = 10;
-        }else if(score===80){
-          level++;
-          $level.text(level);
-          numMargin++;
-          num-=5;
-        }else if(score===90){
-          level++;
-          $level.text(level);
-        } else if(score===100){
-          $grid.hide();
-          $('.final').css('visibility', 'visible');
-          $level.hide();
-          $score.hide();
-          $('h2').hide();
-
-        }
-        $score.text(score);
+        displayingScore();
         $coin.css('margin-top', 0);
-        speed = Math.ceil(Math.random() * 10) + num;//decrease the number by 1 for next level
+        speed = generateSpeed(num);//decrease the number by 1 for next level
+        checkingScore();
       } else {
-        $coin.css('margin-top', marginTop + numMargin);//add more to increase the speed
+        $coin.css('margin-top', marginTop + 2);//add more to increase the speed
       }
-      dropBall($coin, speed);
-    },speed);
+      if(!loseCondition()) dropCoins($coin, speed);
+    }, speed);
     timers.push(timerId);
   }
 
-
+  function generateSpeed(num) {
+    return Math.ceil(Math.random() * 3) + num;
+  }
 
   function startButton(){
     $coin.toArray().forEach((ball, index) => {
       setTimeout(() => {
-        dropBall($(ball), Math.ceil(Math.random() * 10) + 10);
+        dropCoins($(ball), generateSpeed(num));
       }, 1000 * index);
     });
   }
-
-
-
+  $(document).keydown(function(event) {
+    switch (event.keyCode) {
+      case 37: return changingClass(0, 'left');
+      case 39: return changingClass(3, 'right');
+    }
+  });
   $button.on('click', () =>{
     startButton();
     $intro.hide();
     $grid.css('visibility', 'visible');
+    $('h2').css('visibility', 'visible');
   });
 }); //leave this
